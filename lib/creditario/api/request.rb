5895f9d6eb6f8d3fd7c4e176832710d6a33ae162
Eduardo Figarola <eduardofigarola@gmail.com>
2018-06-27 16:38:25 -0500

    Add Creditario::API modules definitions

---
+++ lib/creditario/api/request.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Creditario # :nodoc:
+  module API # :nodoc:
+    ###
+    # == Creditario::API::Request
+    #
+    # Modulo encargado de ejecutar los requests HTTP de cualquier tipo
+    # a la API de creditar.io
+    module Request
+    end
+  end
+end
---
+++ lib/creditario/api/update.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Creditario # :nodoc:
+  module API
+    ###
+    # == Creditario::API::Update
+    #
+    # Modulo encargado de actualizar recursos existentes en la API de creditar.io
+    module Update
+    end
+  end
+end
