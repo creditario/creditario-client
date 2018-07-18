46133525e58423c3ff43c8ec20a819fca5c1acba
memoxmrdl <jmemox@gmail.com>
2018-07-18 10:57:28 -0500

    Add process to delete resource

--- lib/creditario/api/delete.rb
+++ lib/creditario/api/delete.rb
@@ -7,6 +7,17 @@ module Creditario # :nodoc:
     #
     # Modulo encargado de eliminar recursos existentes en la API de creditar.io
     module Delete
+      ###
+      # Realiza una llamada DELETE al path del Repositorio que este haciendo uso
+      # de este modulo.
+      #
+      # Si todo sale bien se ha completado con éxito, pero su respuesta no tiene ningún contenido.
+      def delete(id)
+        path = [self.resource_path, id].join("/")
+        API.request(:delete, path)
+      rescue Creditario::Exceptions::ResourceNotFoundError => exception
+        exception.server_response
+      end
     end
   end
 end
