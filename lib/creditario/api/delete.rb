7b85034bb0539c3ac5e0aa9f46f063c53cc5ec04
memoxmrdl <jmemox@gmail.com>
2018-07-18 11:17:51 -0500

    Change review

--- lib/creditario/api/delete.rb
+++ lib/creditario/api/delete.rb
@@ -11,7 +11,9 @@ module Creditario # :nodoc:
       # Realiza una llamada DELETE al path del Repositorio que este haciendo uso
       # de este modulo.
       #
-      # Si todo sale bien se ha completado con éxito, pero su respuesta no tiene ningún contenido.
+      # Si todo sale bien, la respuesta no tendrá contenido alguno y el código de estado HTTP será 204.
+      # De lo contrario, el código de estado HTTP será diferente a 204 y la respuesta tendrá los detalles
+      # del error.
       def delete(id)
         path = [self.resource_path, id].join("/")
         API.request(:delete, path)
