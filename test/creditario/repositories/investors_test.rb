b4b54bca487f1490409f6026688405f6a3deb7b7
David Padilla <83060+dabit@users.noreply.github.com>
2021-06-09 16:01:46 -0500

    Fix tests for investors

--- test/creditario/repositories/investors_test.rb
+++ test/creditario/repositories/investors_test.rb
@@ -19,7 +19,7 @@ class Creditario::InvestorsTest < CreditarioAPITest
     }
   end
 
-  def test_it_list_investors
+  def test_it_lists_investors
     stub_request(:get, build_api_uri("investors")).
       with(headers: @headers).
       to_return(use_fixture("GET-Investors-200"))
@@ -30,7 +30,7 @@ class Creditario::InvestorsTest < CreditarioAPITest
     assert_equal result.items.size, 2
   end
 
-  def test_it_retrieves_a_investor
+  def test_it_retrieves_an_investor
     stub_request(:get, build_api_uri("investors", "82875f86-2d4e-4c4e-b0a4-7d2c4a4ca52f")).
       with(headers: @headers).
       to_return(use_fixture("GET-Investor-200"))
@@ -51,7 +51,7 @@ class Creditario::InvestorsTest < CreditarioAPITest
     assert result.has_key? "errors"
   end
 
-  def test_it_creates_a_investor
+  def test_it_creates_an_investor
     stub_request(:post, build_api_uri("investors")).
       with(headers: @headers).
       to_return(use_fixture("POST-Investor-201"))
@@ -69,48 +69,5 @@ class Creditario::InvestorsTest < CreditarioAPITest
     result = @subject.create(@investor_params)
 
     assert result.is_a? Creditario::Investor
-  end
-
-  def test_it_updates_a_investor
-    stub_request(:patch, build_api_uri("investors", "2e9d05b8-2180-4779-bab6-bdfd41d1569f")).
-      with(headers: @headers).
-      to_return(use_fixture("PATCH-Investor-202"))
-
-    result = @subject.update("2e9d05b8-2180-4779-bab6-bdfd41d1569f", @investor_params)
-
-    assert result.is_a? Creditario::Investor
-  end
-
-  def test_it_updates_blocked_investor
-    stub_request(:patch, build_api_uri("investors", "2e9d05b8-2180-4779-bab6-bdfd41d1569f")).
-      with(headers: @headers).
-      to_return(use_fixture("PATCH-Investor-403"))
-
-    result = @subject.update("2e9d05b8-2180-4779-bab6-bdfd41d1569f", @investor_params)
-
-    assert result.is_a? Hash
-    assert result.has_key? "errors"
-  end
-
-  def test_it_updates_investor_fail
-    stub_request(:patch, build_api_uri("investors", "2e9d05b8-2180-4779-bab6-bdfd41d1569f")).
-      with(headers: @headers).
-      to_return(use_fixture("PATCH-Investor-422"))
-
-    result = @subject.update("2e9d05b8-2180-4779-bab6-bdfd41d1569f", @investor_params)
-
-    assert result.is_a? Hash
-    assert result.has_key? "errors"
-  end
-
-  def test_it_updates_investor_not_found
-    stub_request(:patch, build_api_uri("investors", "2e9d05b8-2180-4779-bab6-bdfd41d1569f")).
-      with(headers: @headers).
-      to_return(use_fixture("PATCH-Investor-404"))
-
-    result = @subject.update("2e9d05b8-2180-4779-bab6-bdfd41d1569f", @investor_params)
-
-    assert result.is_a? Hash
-    assert result.has_key? "errors"
   end
 end
